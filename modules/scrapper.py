ce65ad0e4f38122bd5ed847baabf16e2d090b701
OutofRange-Nico <83180802+OutofRange-Nico@users.noreply.github.com>
2021-05-07 15:47:20 +0200

    Some Changes

--- modules/scrapper.py
+++ modules/scrapper.py
@@ -1,44 +1,87 @@
+from typing import Any
 import requests
+from requests.models import Response
 from bs4 import BeautifulSoup
 
 
 class Scrapper:
-    def __init__(self, urls=None, contents=[]):
+    """
+    Scrapper Class
+    """
+    def __init__(self, urls: object = None, contents: list = []) -> None:
+        """Contructor
+
+        Args:
+            urls (object, optional): [description]. Defaults to None.
+            contents (list, optional): [description]. Defaults to [].
+        """
+
         self.urls = urls
+
         if urls is not None:
             if self.urls[0][-1] != "/":
                 self.urls[0] = str(self.urls[0]).removesuffix("/")
+
         self.contents = contents
 
-    def clean(self):
-        contents = []
+    def clean(self) -> list:
+        """clean function
+
+        Returns:
+            list: [description]
+        """
+
+        contents: list = []
+
         for content in self.contents:
-            soup = BeautifulSoup(content, "html.parser")
+            soup: any = BeautifulSoup(content, "html.parser")
+
             for script in soup(["script", "style"]):
                 script.extract()
-            cleaned = soup.get_text()
-            lines = (line.strip() for line in cleaned.splitlines())
-            chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+
+            cleaned: object = soup.get_text()
+            lines: object = (line.strip() for line in cleaned.splitlines())
+            chunks: object = (phrase.strip() for line in lines for phrase in line.split("  "))
             contents.append('\n'.join(chunk for chunk in chunks if chunk))
+
         return contents
 
-    def getURLs(self):
-        urls = []
+    def getURLs(self) -> list:
+        """getURLs function
+
+        Returns:
+            list: [description]
+        """
+        
+        urls: list = []
+
         for content in self.contents:
             soup = BeautifulSoup(content, "html.parser")
+
             for link in soup.find_all('a'):
                 if link.get("href") is not None:
                     if self.urls[0] not in link.get("href"):
                         if "http" not in link.get("href") and "https" not in link.get("href") and "mailto:" not in link.get("href"):
                             urls.append(self.urls[0] + link.get('href'))
                             continue
-                urls.append(link.get("href"))
-        return list(dict.fromkeys(urls))
 
-    def getText(self):
-        contents = []
+                urls.append(link.get("href"))
+
+        return urls
+
+    def getText(self) -> list:
+        """getText function
+
+        Returns:
+            list: [description]
+        """
+
+        contents: list = []
+
         for url in self.urls:
-            req = requests.get(url)
+            req: Response = requests.get(url)
             contents.append(req.text)
+
         contents = Scrapper(contents=contents).clean()
+
         return contents
