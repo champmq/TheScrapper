ba1ba06e174a02cb21a3b2ba61213b5093827f4b
Champ <40720714+champmq@users.noreply.github.com>
2021-09-06 18:29:04 +0200

    fix for regex

--- modules/info_reader.py
+++ modules/info_reader.py
@@ -23,7 +23,7 @@ class InfoReader:
         self.content: object = content
         self.social_path: object = social_path
         self.res: dict =  {
-            "phone": "^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$",
+            "phone": "/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,7}$/gm",
             "email": r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
         }
 
